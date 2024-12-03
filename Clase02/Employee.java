29c16d683019f0dcfd8e81ad428324fe0cdcca87
Nicolas Manjarres Gonzalez <nicolasmanjarresg@gmail.com>
2024-12-02 22:48:57 -0500

    initial commit

--- Clase02/Employee.java
+++ Clase02/Employee.java
@@ -63,8 +63,43 @@ public class Employee {
     public String toString() {  //showThis
         return "Employee [name=" + name + ", id=" + id + ", salary=" + salary + "]";
     }
- 
 
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + id;
+        long temp;
+        temp = Double.doubleToLongBits(salary);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Employee other = (Employee) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (id != other.id)
+            return false;
+        if (Double.doubleToLongBits(salary) != Double.doubleToLongBits(other.salary))
+            return false;
+        return true;
+    }
+
+    
 
 
 
--- Clase02/Main.java
+++ Clase02/Main.java
@@ -5,12 +5,16 @@ public class Main {
     public static void main(String[] args) {
 
         Employee empleado1 = new Employee("Nicolas", 1564, 150000);
+        Employee empleado2 = new Employee("Manjarres", 5454, 150000);
         
         
+        System.out.println(empleado1.toString());
+        empleado1.increaseSalary(10);
         System.out.println(empleado1.toString());
 
         empleado1.setSalary(0);
         System.out.println(empleado1.toString());
+        System.out.println(empleado1.hashCode());
 
         
 
